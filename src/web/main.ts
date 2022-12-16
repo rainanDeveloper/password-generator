872548e82f74b2eefe5f25881c9b6d89dbe92eee
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-12-16 12:36:37 -0300

    fix: Fixed the port issue on heorku application

--- src/web/main.ts
+++ src/web/main.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-const APP_PORT = 3000;
+const APP_PORT = Number(process.env['PORT']) || 3000;
 
 app.listen(APP_PORT, '0.0.0.0', () => {
   console.log(`Server running at port ${APP_PORT}`);
