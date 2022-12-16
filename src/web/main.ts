a3cd684696426bbced4cd2e03d38d0ea7597dc47
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-12-16 12:30:09 -0300

    fix: Fixed heroku host binding error

--- src/web/main.ts
+++ src/web/main.ts
@@ -2,6 +2,6 @@ import { app } from './app';
 
 const APP_PORT = 3000;
 
-app.listen(APP_PORT, () => {
+app.listen(APP_PORT, '0.0.0.0', () => {
   console.log(`Server running at port ${APP_PORT}`);
 });
