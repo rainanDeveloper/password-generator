c503d4295c17bfda90d405cadb7ee65bd052cb06
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-12-16 15:41:38 -0300

    fix: removed unused imports

--- src/web/app.ts
+++ src/web/app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { Express, Request, Response } from 'express';
+import { Express } from 'express';
 import * as cors from 'cors';
 import { Router } from './routes/router';
 import { join } from 'path';
